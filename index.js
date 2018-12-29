00a2e13d32419eecc82b53e95c73440c59e60350
darkship <crumble_tk@hotmail.com>
2018-12-29 20:49:42 +0100

    fix multiple title from different sources

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-require('./globals.js')
+require("./globals.js");
 
-const {rss, torrentTitles, transmissionConfig} = config;
-const TorrentFeedLoader = require('./lib/torrent_feed_loader')
-const Transmission = require('transmission-client').Transmission;
+const {rss, transmissionConfig} = config;
+const TorrentFeedLoader = require("./lib/torrent_feed_loader");
+const Transmission = require("transmission-client").Transmission;
 
-const rexps = torrentTitles.map(t => new RegExp(t))
-const lastRunFile = __dirname + '/lastrun';
+const lastRunFile = __dirname + "/lastrun";
 
 const Client = new Transmission(transmissionConfig);
+
+let clientError = false;
 
 const add = async ({title, link}, method) => {
   try {
     const addRes = await Client[method](link);
+    await Client.start([addRes.id]);
     logger.info(`added: ${title}`);
-    await Client.start([addRes.id])
   } catch (e) {
-    logger.error(`${title} - ${e.message}`)
+    logger.error(`${title} - ${e.message}`);
+    if (e.message !== "duplicate torrent") {
+      clientError = true;
+    }
   }
 };
 
@@ -23,18 +27,21 @@ const tfl = new TorrentFeedLoader({rss, lastRunFile});
 
 tfl.loadLastRun();
 
-tfl.on('loadLastRun', (date) => {
-  logger.info('last successful run was', date);
-  tfl.startFetchTorrents(date)
+tfl.on("loadLastRun", (date) => {
+  logger.info("last successful run was", date);
+  tfl.startFetchTorrents(date);
 });
 
-tfl.on('torrentFeed', ({title, link}, {method}) => {
-  const isWantedTorrent = rexps.find(t => title.match(t));
+tfl.on("torrentFeed", ({title, link}, {method, torrentTitles}) => {
+  const isWantedTorrent = torrentTitles.find(t => title.match(t));
   if (isWantedTorrent) {
     logger.info(`found: ${title}`);
-    add({title, link}, method)
+    add({title, link}, method);
   }
 });
 
-tfl.on('error', error => logger.error(error.message))
-tfl.on('fetchTorrents', t => logger.info('fetchTorrents', t))
+tfl.on("done", () => !clientError && tfl.setLastRun());
+
+tfl.on("error", error => logger.error(error.message));
+tfl.on("fetchTorrents", ({url, method}) => logger.info(`fetchTorrents url=${url}, method=${method}`));
+
